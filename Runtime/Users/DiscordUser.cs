7f0dc6bebd06fb34b9589d56d0d5015d8a6145f4
Danae Dekker <hello@danaedekker.com>
2026-08-17 17:50:43 +0200

    Added support for checking equality on users and if users are playing any game or this game

--- Runtime/Users/DiscordUser.cs
+++ Runtime/Users/DiscordUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using Discord.Sdk;
@@ -8,7 +9,7 @@ namespace Audune.Social.Discord
   /// <summary>
   /// Class that defines a user in the Discord social provider.
   /// </summary>
-  public sealed class DiscordUser : IUser
+  public sealed class DiscordUser : IUser, IEquatable<DiscordUser>
   {
     // Internal state
     private readonly DiscordSocialProvider _socialProvider;
@@ -34,6 +35,15 @@ namespace Audune.Social.Discord
       _ => UserStatus.Offline,
     };
 
+    /// <inheritdoc/>
+    /// <remarks>
+    /// A Discord user only knows if they are playing the current game.
+    /// </remarks>
+    public bool isPlaying => _userHandle.GameActivity() != null;
+
+    /// <inheritdoc/>
+    public bool isPlayingThisGame => isPlaying;
+
 
     /// <summary>
     /// Constructor.
@@ -52,6 +62,63 @@ namespace Audune.Social.Discord
     public UniTask<Texture2D> GetAvatar(int size = 1024, CancellationToken cancellationToken = default)
     {
       return DiscordImageUtils.DownloadAvatarTexture(_userHandle, size, cancellationToken);
+    }
+    #endregion
+    
+    #region Equatable implementation
+    /// <inheritdoc/>
+    public override bool Equals(object obj)
+    {
+      return ReferenceEquals(this, obj) 
+        || obj is IUser other && Equals(other);
+    }
+    
+    /// <inheritdoc/>
+    public bool Equals(IUser other)
+    {
+      return ReferenceEquals(this, other) 
+        || other is DiscordUser discordUser && Equals(discordUser);
+    }
+    
+    /// <inheritdoc/>
+    public bool Equals(DiscordUser other)
+    {
+      if (other is null)
+        return false;
+      if (ReferenceEquals(this, other))
+        return true;
+
+      return Equals(_socialProvider, other._socialProvider)
+        && Equals(_userHandle.Id(), other._userHandle.Id());
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+      return HashCode.Combine(_socialProvider, _userHandle.Id());
+    }
+
+    
+    /// <summary>
+    /// Returns if the specified Discord users equal each other.
+    /// </summary>
+    /// <param name="left">The left Discord user to check.</param>
+    /// <param name="right">The right Discord user to check.</param>
+    /// <returns>If the specified Discord users equal each other.</returns>
+    public static bool operator ==(DiscordUser left, DiscordUser right)
+    {
+      return Equals(left, right);
+    }
+
+    /// <summary>
+    /// Returns if the specified Discord users do not equal each other.
+    /// </summary>
+    /// <param name="left">The left Discord user to check.</param>
+    /// <param name="right">The right Discord user to check.</param>
+    /// <returns>If the specified Discord users do not equal each other.</returns>
+    public static bool operator !=(DiscordUser left, DiscordUser right)
+    {
+      return !(left == right);
     }
     #endregion
   }
